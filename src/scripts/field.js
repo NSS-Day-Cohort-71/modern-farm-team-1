ad7404d9493528121510ba482a9faae8d6f7103a
kat coveney <katcoveney.author@gmail.com>
2024-06-04 15:16:18 -0500

    fixed field function syntax

---
+++ src/scripts/field.js
@@ -0,0 +1,15 @@
+let field = [] // array where the seeds will be planted
+
+// function to add plants to the field
+export const addPlant = (seeds) => {
+    if (Array.isArray(seeds)) {
+        for (const s of seeds) { // adds each item individually if the seed is part of an array (like corn)
+            field.push(s)
+        }
+    } else {
+        field.push(seeds)
+    }
+}
+
+// function to use the seeds (return a copy of the field array)
+export const usePlants = () => field.slice()
