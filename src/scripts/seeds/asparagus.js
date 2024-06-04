51dd34a23f02e8130ffbd945a5a2bb6ecafddd5a
Jeremy Whitney <bearwhitness@gmail.com>
2024-06-04 14:24:55 -0500

    Commit ft/seeds_modules and diagrams

---
+++ src/scripts/seeds/asparagus.js
@@ -0,0 +1,8 @@
+// Define and export createAsparagus function
+export const createAsparagus = () => (
+    {
+        type: "Asparagus",
+        height: 25,
+        output: 4
+    }
+)
---
+++ src/scripts/seeds/sunflower.js
@@ -0,0 +1,8 @@
+// Define and export createSunflower function
+export const createSunflower = () => (
+    {
+        type: "Sunflower",
+        height: 380,
+        output: 3
+    }
+)
