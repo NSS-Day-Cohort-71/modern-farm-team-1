90916a27010904820f19940f622d94c060f8c388
kat coveney <katcoveney.author@gmail.com>
2024-06-06 13:20:15 -0500

    fixed catalog issue

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,15 +1,11 @@
-    // define and export a catalog function
-        //accept harvested food array as input
-        //iterate array of food objects
-        //append <main> with innerHTML representation of food items
-        // add <img src="${food.imgURL}" alt="${food.type}"> into the section thats added to mainELHtml 
-        //to add an image instead of a name to the display
-    export const catalog = (foodArr) => {
-        let mainElHtml = ``
-        foodArr.forEach(food => {
-            mainElHtml += `
-                <section class="plant">${food.type}:${food.output}</section>
-            ` 
-            });
-            return mainElHtml
-    }
+//Accepts harvestedCrops or sortedCrops
+
+export const catalog = (foodArr) => {
+    let mainElHtml = ``
+    foodArr.forEach(food => {
+        mainElHtml += `
+            <section class="plant">${food.type}</section>
+        ` 
+    });
+    return mainElHtml
+}
